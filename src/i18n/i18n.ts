d9405422be9120474a5505dd0aa9d0912be1a53f
Alejandro Castañeda García <al148506@gmail.com>
2026-08-20 12:28:06 -0600

    Configure Spanish as the sole application language

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,18 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import es from "./es.json";
-import en from "./en.json";
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
       es: { translation: es },
-      en: { translation: en },
     },
+    lng: "es",
     fallbackLng: "es",
+    supportedLngs: ["es"],
     interpolation: {
       escapeValue: false,
     },
